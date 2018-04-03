0e9a3d3b55b06b8d04e4d1c6441c66062b0ec7aa
Palacios Rodriguez <francisco-javier.palaciosrodriguez.external@atos.net>
2018-04-03 16:03:11 +0100

    Spring security

--- src/main/java/com/atos/coderank/configuration/WebSecurityConfig.java
+++ src/main/java/com/atos/coderank/configuration/WebSecurityConfig.java
@@ -1,11 +1,52 @@
 package com.atos.coderank.configuration;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import static com.atos.coderank.configuration.SecurityConstants.LOGIN_URL;
 
-@Configuration
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	
+	@Autowired
+	private UserDetailsService userDetailsService;
 
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors()
+			.and()
+			.csrf().disable()
+			.authorizeRequests().antMatchers(HttpMethod.POST, LOGIN_URL).permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.addFilter(new JWTAuthenticationFilter(authenticationManager()))
+			.addFilter(new JWTAuthorizationFilter(authenticationManager()))
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
 }
--- src/main/java/com/atos/coderank/controllers/LoginController.java
+++ src/main/java/com/atos/coderank/controllers/LoginController.java
@@ -2,17 +2,8 @@ package com.atos.coderank.controllers;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.atos.coderank.models.UserModel;
-import com.atos.coderank.services.LoginService;
 
 @RestController
 @RequestMapping("api/auth")
@@ -20,14 +11,5 @@ public class LoginController {
 
 	private final static Log LOG = LogFactory.getLog(LoginController.class);
 	
-	@Autowired
-	@Qualifier("loginService")
-	private LoginService ls;
-		
-	@PostMapping("")
-	public ResponseEntity<String> login(@RequestBody UserModel user){
-		String token = this.ls.findByDasAndPassword(user.getDas(), user.getPassword());
-		
-		return new ResponseEntity<>(token, HttpStatus.OK);
-	}
+
 }
